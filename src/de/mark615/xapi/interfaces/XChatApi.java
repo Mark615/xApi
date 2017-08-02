f8dbdf8c0bd551ac81e5dee5bf5db1a5230fccfe
Mark615 <you2maxi@t-online.de>
2017-08-02 18:07:08 +0200

    xchat

--- src/de/mark615/xapi/interfaces/XChatApi.java
+++ src/de/mark615/xapi/interfaces/XChatApi.java
@@ -1,5 +1,7 @@
 package de.mark615.xapi.interfaces;
 
+import java.util.UUID;
+
 import de.mark615.xapi.XApi;
 
 public abstract class XChatApi extends XPlugin implements XChatApiInterface
@@ -7,6 +9,35 @@ public abstract class XChatApi extends XPlugin implements XChatApiInterface
 	public XChatApi(XApi plugin)
 	{
 		super(plugin);
+	}
+	
+	
+	//mute
+	public boolean isPlayerMuted(UUID uuid)
+	{
+		return false;
+	}
+	
+	public boolean isWorldMuted(String world)
+	{
+		return false;
+	}
+	
+	public boolean isServerMuted(String server)
+	{
+		return false;
+	}
+	
+	public boolean isChatroomMuted(String name)
+	{
+		return false;
+	}
+	
+	
+	//ban
+	public boolean isPlayerBanned(UUID uuid)
+	{
+		return false;
 	}
 
 }
